bd526f91418cf55ff0ab964fa7f8016361f08216
jomapormentilla <jomapormentilla@gmail.com>
2020-10-26 17:31:27 -0400

    commit tree ebbf37f01410a969946188331c827a30b6bd8934

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name( name )
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name( name )
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name( name )
+    self.all.find{ |song|
+      song.name == name
+    }
+  end
+
+  def self.find_or_create_by_name( name )    
+    if find_by_name( name ) != nil
+      find_by_name( name )
+    else
+      create_by_name( name )
+    end
+  end
+
+  def self.alphabetical
+    # Cool, you can sort by attribute!
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename( string )
+    song = self.new
+    arr = string.split(/[-||.]/)
+    song.name = arr[1].strip
+    song.artist_name = arr[0].strip
+    self.all << song
+    song
+  end
+
+  def self.create_from_filename( string )
+    song = new_from_filename( string )
+    self.all << song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
